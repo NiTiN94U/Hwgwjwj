6c767bcf1aa97a07290ee987e3abfee55f562386
NiTiN94U <nitin992516@gmail.com>
2025-03-09 19:37:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from telegram.ext import Application
+from handlers import start, button_handler
+from config import TOKEN
 
+def main():
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CallbackQueryHandler(button_handler))
+
+    print("Bot is running...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
